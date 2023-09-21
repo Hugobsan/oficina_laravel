3c91c80aca1d4cc05fc700b1d3aad5c8569af9cf
Hugobsan <hugocorone10@gmail.com>
2023-09-20 21:38:51 -0300

    initial commit

---
+++ biblioteca-agora/database/migrations/2023_09_21_000937_create_livro_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('livro', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo', 255)->index();
+            $table->integer('volume');
+            $table->integer('edicao');
+            $table->integer('numero_paginas');
+            $table->string('isbn', 13)->unique();
+            $table->string('editora', 255);
+            $table->int('quantidade');
+            $table->unsignedBigInteger('autor_id');
+            $table->unsignedBigInteger('genero_id');
+            $table->timestamps();
+
+            // Foreign keys
+            $table->foreign('autor_id')->references('id')->on('autor');
+            $table->foreign('genero_id')->references('id')->on('genero');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('livro');
+    }
+};
---
+++ biblioteca-agora/database/migrations/2023_09_21_001751_create_emprestimo_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('emprestimo', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('locatario_id');
+            $table->unsignedBigInteger('livro_id');
+            $table->date('data_emprestimo');
+            $table->date('data_devolucao_esperada');
+            $table->int('quantidade_renovacoes')->default(0);
+            $table->date('data_devolucao')->nullable()->default(null);
+            $table->timestamps();
+
+            // Foreign keys
+            $table->foreign('locatario_id')->references('id')->on('locatario');
+            $table->foreign('livro_id')->references('id')->on('livro');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('emprestimo');
+    }
+};
