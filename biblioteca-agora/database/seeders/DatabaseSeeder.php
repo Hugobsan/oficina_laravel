4268c9568eddc37ef8355e54c81a7540eee1f71c
Hugo Barbosa Santos <hugocorone10@gmail.com>
2023-09-21 09:14:20 -0300

    Removendo seeders do database seeder por segurança

--- biblioteca-agora/database/seeders/DatabaseSeeder.php
+++ biblioteca-agora/database/seeders/DatabaseSeeder.php
@@ -12,7 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(create_locatario_seeder::class);
-        $this->call(create_livros_seeder::class);
+
     }
 }
